6d6b18938cec1cdc85bc58aee5cedf2139664933
pasatron10 <36640332+pasatron10@users.noreply.github.com>
2019-12-15 13:51:42 +0100

    Huma ficat, script camera i humá

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public int stop;
+    public float speed;
+    int start = 0;
+
+    void Update()
+    {
+        var step = speed * Time.deltaTime;
+        start += 1;
+        if(start <= stop)transform.Rotate(0, -step, 0); 
+    }
+}
---
+++ Assets/VelocityManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityManager : MonoBehaviour
+{
+    Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        if(_rb)
+        {
+            _rb.velocity = Vector3.forward;
+        }
+    }
+}
